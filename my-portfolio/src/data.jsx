0eb1f8ff6762b5574db533cd0f0e689e70139502
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-07-29 23:19:58 +0430

    add data js

---
+++ my-portfolio/src/data.jsx
@@ -0,0 +1,78 @@
+
+export const projects = [
+    {
+        title: "React Reserve",
+        subtitle: "MERN Stack",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: './assets/goki.jpg',
+        link: "https://reactbootcamp.com",
+    },
+    {
+        title: "React Tracks",
+        subtitle: "React and Python",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-2.gif",
+        link: "https://reedbarger.com",
+    },
+    {
+        title: "DevChat",
+        subtitle: "React and Firebase",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-3.gif",
+        link: "https://jsbootcamp.com",
+    },
+    {
+        title: "Epic Todo App",
+        subtitle: "React Hooks",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-4.gif",
+        link: "https://pythonbootcamp.com",
+    },
+];
+
+export const skills = [
+   "JavaScript",
+   "TypeScript",
+    "Nodejs",
+    "Express",
+   "Reactjs",
+   "React Native",
+]
+export const testimonials = [
+    {
+        name: "Epic Todo App",
+        quote:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-4.gif",
+        link: "https://pythonbootcamp.com",
+        company:""
+    },
+    {
+        name: "Epic Todo App",
+        quote:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-4.gif",
+        link: "https://pythonbootcamp.com",
+        company:""
+    },
+    {
+        name: "Epic Todo App",
+        quote:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-4.gif",
+        link: "https://pythonbootcamp.com",
+        company:""
+    },
+    {
+        name: "Epic Todo App",
+        quote:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-4.gif",
+        link: "https://pythonbootcamp.com",
+        company:""
+    },
+]
